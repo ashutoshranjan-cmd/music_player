b356cc7ab71fadc8b8eb7f7e6271f46b6a53931e
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2022-04-16 05:45:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Gintama",
+        artist: "song 1",
+        img: "gin1",
+        src: "music-1"
+    },
+    {
+        name: "Gintama",
+        artist: "song 2",
+        img: "gin1",
+        src: "music-2"
+    },
+    {
+        name: "Gintama",
+        artist: "song 3",
+        img: "gin1",
+        src: "music-3"
+    },
+    {
+        name: "Gintama",
+        artist: "song 4",
+        img: "gin1",
+        src: "music-4"
+    },
+    {
+        name: "Gintama",
+        artist: "song 5",
+        img: "gin1",
+        src: "music-5"
+    },
+    {
+        name: "Gintama",
+        artist: "song 6",
+        img: "gin1",
+        src: "music-6"
+    },
+    {
+        name: "Gintama",
+        artist: "song 7",
+        img: "gin1",
+        src: "music-7"
+    },
+    {
+        name: "Gintama",
+        artist: "song 8",
+        img: "gin1",
+        src: "music-8"
+    },
+    {
+        name: "Gintama",
+        artist: "song 9",
+        img: "gin1",
+        src: "music-9"
+    },
+    {
+        name: "Gintama",
+        artist: "song 10",
+        img: "gin1",
+        src: "music-10"
+    },
+]
